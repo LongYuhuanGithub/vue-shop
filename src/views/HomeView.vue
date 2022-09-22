13d8cdf54b0607ba868eadc75fc4a0fd960336fd
longyuhuan <2630819701@qq.com>
2022-09-22 09:46:43 +0800

    添加一个退出功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="home">
-    <el-button type=""></el-button>
+    <el-popconfirm title="确认要退出吗？" @confirm="logout">
+      <template #reference>
+        <el-button type="info">退出</el-button>
+      </template>
+    </el-popconfirm>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    logout() {
+      sessionStorage.removeItem('vue_shop_token')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
